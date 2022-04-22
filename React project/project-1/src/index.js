daf77cfa7860af77b7baef577bdcc5080aa8eeb1
MUKUND THAKUR <mukuthakur4008@gmail.com>
2022-04-22 13:17:54 +0530

    coding challenge 1

--- React project/project-1/src/index.js
+++ React project/project-1/src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const time=new Date();
+const colorStyle={
+  color:""
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>hello world!</h1>
-  </React.StrictMode>
+var greeting="morning";
+if(time.getHours()>6 && time.getHours()<12){
+  greeting="morning";
+  colorStyle.color='red';
+}else if(time.getHours()>12 && time.getHours()<18){
+  greeting="afternoon";
+  colorStyle.color='green';
+}else{
+  greeting="night";
+  colorStyle.color='blue';
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="greet" style={colorStyle}>{"Good"+" "+greeting}</h1>
+  </div>,
+  document.getElementById("root")
 );
-
