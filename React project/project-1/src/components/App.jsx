1f06ff51ffffd1d63024fa65ddc43a08f14d7307
MUKUND THAKUR <mukuthakur4008@gmail.com>
2022-04-23 21:46:50 +0530

    props example

--- React project/project-1/src/components/App.jsx
+++ React project/project-1/src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import * as heading from "./Heading";
-import List from "./List";
+// import * as heading from "./Heading";
+// import List from "./List";
 
 function App() {
   return (
-    <div>
-      <h1>{heading.HEeading()}</h1>
-      <h1>{heading.head()}</h1>
-      <List />
-    </div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">Virat Kohli</h2>
+          <img className="circle-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Virat_Kohli.jpg/220px-Virat_Kohli.jpg" alt="Cricketer" />
+        </div>
+        <div className="bottom">
+          <p className="info" >Telephone no: +1234567890</p>
+          <p className="info" >mail : vk12@mail.com</p>
+        </div>
+      </div>
   );
 }
 export default App;
--- React project/project-1/src/index.js
+++ React project/project-1/src/index.js
@@ -1,31 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./components/App";
+import App from "./components/App";
 
-function Card(props) {
-  return (<div>
-      <h1>{props.name}</h1>
-      <img src={props.imga} alt="king" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-  </div>);
-}
-ReactDOM.render
-    (<div>
-        <h1>
-            Contact List
-        </h1>
-        <Card 
-            name="Virat Kohli"
-            imga="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Virat_Kohli.jpg/220px-Virat_Kohli.jpg"
-            tel="+1234567890"
-            email="V.kohli@gmail.com"
-        />
-        <Card 
-            name="jos the boss"
-            imga="https://www.cricbuzz.com/a/img/v1/152x152/i1/c170980/jos-buttler.jpg"
-            tel="+9999888783"
-            email="josthebosh@mail.in"
-        />
-    </div>, 
-document.getElementById("root"));
+// function Card(props) {
+//   return (<div>
+//       <h1>{props.name}</h1>
+//       <img src={props.imga} alt="king" />
+//       <p>{props.tel}</p>
+//       <p>{props.email}</p>
+//   </div>);
+// }
+
+ReactDOM.render(<App />,document.getElementById("root"));
