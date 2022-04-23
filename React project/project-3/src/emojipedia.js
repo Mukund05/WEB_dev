5ff10bc60b9e8c4a776f96522f1c1d22151b9455
MUKUND THAKUR <mukuthakur4008@gmail.com>
2022-04-24 01:05:52 +0530

    project 3 emojipedia

---
+++ React project/project-3/src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia=[
+    {
+        id:1,
+        emoji:"🤑",
+        name:"Money-Mouth Face",
+        meaning:"Used for a wide range of content dealing with money, including: making money, loving wealth, being or feeling rich, and concepts of success and excellence"
+    },
+    {
+        id:2,
+        emoji:"😪",
+        name:"Sleepy Face",
+        meaning:"indicate a character is tired or sleeping in anime or manga. Not to be confused with Sleeping Face "
+    },
+    {
+        id:3,
+        emoji:"🥴",
+        name:"Woozy Face",
+        meaning:"A yellow face with a crumpled mouth and a cockeyed expression, as if tired and emotional from inebriation or smitten with love. Depicted with raised or furrowed eyebrows and at least one eye half-open."
+    },
+    {
+        id: 4,
+        emoji: "🤓",
+        name: "Nerd Face",
+        meaning:"Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+        id:5,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        meaning:"This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id:6,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning:"Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+        id:7,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning:"“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    }
+];
+
+export default emojipedia;
